6757c2bbaef57f6445d01c19328cbd6695e55591
jv95 <10341166+jv95@users.noreply.github.com>
2018-01-14 01:39:41 +0100

    trochu jsem si upravil kod..

--- src/GuessMovies.java
+++ src/GuessMovies.java
@@ -7,21 +7,22 @@ import java.util.Scanner;
 public class GuessMovies {
 
     public static void main(String [] args) throws Exception {
-        int amountOfLines = 0;
-        File file = new File("src/movies.txt");
-        BufferedReader reader = new BufferedReader(new FileReader(file));
+        int amountOfLines = 0; // variable for amount of lines in file
+        File file = new File("src/movies.txt"); // file with movie names
+        BufferedReader reader = new BufferedReader(new FileReader(file));   // reader for counting the lines in file
+        BufferedReader br = new BufferedReader(new FileReader(file));   // reader for storing the line in array
+        String line = "";   // variable for storing the current line in while loop
+        int i = 0;  // variable for while loop
+        char[] movieChars; //array for chosen movie
+        Scanner scannerS = new Scanner(System.in);  // scanner for reading the user letter guess
+
         //count amount of lines to make the array only as big as needed
         while (reader.readLine() !=null){
             amountOfLines++;
         }
+        String [] moviesList = new String[amountOfLines];   //array for storing the movie names
 
-        String [] moviesList = new String[amountOfLines];
-        char[] movieChars;
-
-        int i = 0;
-        String line = "";
-        BufferedReader br = new BufferedReader(new FileReader(file));
-            while ((line = br.readLine())!=null) {
+        while ((line = br.readLine())!=null) {
                 //write down the name
                 moviesList[i] = line;
                 //increment i
@@ -30,9 +31,8 @@ public class GuessMovies {
 
         //choose a random movie
         int randomMovie = (int) ((Math.random() * amountOfLines)+1);
-           String finalMovie = (moviesList[randomMovie]);
-            //fill movieChars with the letters
-
+        String finalMovie = (moviesList[randomMovie]);
+        //fill movieChars with the letters
         movieChars = finalMovie.toCharArray();
         //get number of letters of a chosen movie
         int numberOfLetters = movieChars.length;
@@ -50,27 +50,21 @@ public class GuessMovies {
             //tell the user to guess
             System.out.println("Guess a letter");
             //read the input
-            Scanner scannerS = new Scanner(System.in);
             String input = scannerS.next().toString().toLowerCase();
             //make it a char if ti isn't
             char guess = input.charAt(0);
             //limit the number of guesses
             //check the title for the input
-
             for (int s = 0; s < numberOfLetters; s++) {
                 if (guess == movieChars[s]) {
                     //replace _  with the char in a correct position
                     secretWord[s] = guess;
                     remainingLetters--;
                 }
+                System.out.print(remainingLetters);
             }
             System.out.println(Arrays.toString(secretWord));
         }
-
-            System.out.println("Thats right! It is " + finalMovie + ".");
+        System.out.println("Thats right! It is " + finalMovie + ".");
         }
         }
-
-
-
-
